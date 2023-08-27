a940233d6e957b9375f2dbe6dce027605118789c
makswelll <nikolaevic21.maks@gmail.com>
2023-08-27 21:27:01 +0300

    task-8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
-
+const loginFormRef = document.querySelector(".login-form");
+// console.log(loginFormRef);
+loginFormRef.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formData = new FormData(loginFormRef);
+  const formDataObject = {};
+  formData.forEach((value, name) => {
+    formDataObject[name] = value;
+  });
+  if (!formDataObject.email || !formDataObject.password) {
+    alert("All fields must be filled in");
+    return;
+  }
+  console.log(formDataObject);
+  loginFormRef.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorSpanRef = document.querySelector(".color");
+const changeColorRef = document.querySelector(".change-color");
+const bodyRef = document.body;
+
+changeColorRef.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = randomColor;
+  colorSpanRef.textContent = randomColor;
+});
